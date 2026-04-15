01d077a5218f9ff701dfd948e2cc315f02b4cde3
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-04-15 16:12:01 +0530

    Roboshop Project

--- inputs.sh
+++ inputs.sh
@@ -5,3 +5,13 @@ echo "All Values - * - $*"
 echo " no of Values # - $#"
 
 echo exported variable name - $name
+
+
+sample(){
+  echo LHS Values a - $a , b - $b
+
+  echo RHS Values 1 - $1 , 2 - $2
+  echo "All Values - * - $*"
+  echo " no of Values # - $#"
+}
+a=9 b=10 sample
